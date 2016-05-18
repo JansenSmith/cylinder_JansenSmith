788b75656158435297954f8c5731387311f42e36
madhephaestus <mad.hephaestus@gmail.com>
2016-05-17 22:59:30 -0400

    pyrmid

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,8 +1,15 @@
 
 //create a Cylinder
-CSG cylinder = new Cylinder(	10, // Radius at the top
-                      				20, // Radius at the bottom
+CSG cylinder = new Cylinder(	10, // Radius at the bottom
+                      				20, // Radius at the top
                       				40, // Height
                       			         (int)30 //resolution
-                      			         ).toCSG()//convert to CSG to display
- return cylinder
+                      			          ).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
+  //create a Cylinder
+CSG pyramid = new Cylinder(	20, // Radius at the bottom
+                      				0, // Radius at the top
+                      				40, // Height
+                      			         (int)4 //resolution
+                      			         ).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display
+                      			         .movex(50)
+ return [cylinder ,pyramid]
