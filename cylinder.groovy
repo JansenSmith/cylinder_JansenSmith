c68ab73761daa790df3386a0ee2a65afb0418277
madhephaestus <mad.hephaestus@gmail.com>
2016-06-01 14:17:28 -0400

    added a simple cylindar

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,15 +1,17 @@
+CSG simpleSyntax =new Cylinder(10,10,40,(int)30).toCSG() // a one line Cylinder
 
 //create a Cylinder
-CSG cylinder = new Cylinder(	10, // Radius at the bottom
-                      				20, // Radius at the top
-                      				40, // Height
-                      			         (int)30 //resolution
-                      			          ).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
+CSG myCylinder = new Cylinder(10, // Radius at the bottom
+                      		20, // Radius at the top
+                      		40, // Height
+                      		(int)30 //resolution
+                      		).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
+                      		.movey(50)
   //create a Cylinder
 CSG pyramid = new Cylinder(	20, // Radius at the bottom
-                      				0, // Radius at the top
-                      				40, // Height
-                      			         (int)4 //resolution
-                      			         ).toCSG()//convert to CSG to display                    			 
-                      			         .movex(50)
- return [cylinder ,pyramid]
+                      		0, // Radius at the top
+                      		40, // Height
+                      		(int)4 //resolution
+                      		).toCSG()//convert to CSG to display                    			 
+                      		.movex(50)
+ return [simpleSyntax,myCylinder ,pyramid]
