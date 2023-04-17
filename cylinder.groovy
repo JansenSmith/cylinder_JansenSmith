d626eb6b77889976cc152d3aa850d2e4b6c0483a
JansenSmith <jansen@bstarengineering.com>
2023-04-17 16:53:20 -0400

    removed redundancy

--- cylinder.groovy
+++ cylinder.groovy
@@ -5,7 +5,7 @@ CSG myCylinder = new Cylinder(10, // Radius at the bottom
                       		20, // Radius at the top
                       		40, // Height
                       		(int)30 //resolution
-                      		).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
+                      		).toCSG()//convert to CSG to display
                       		.movey(50)
   //create a Cylinder
 CSG pyramid = new Cylinder(	20, // Radius at the bottom
