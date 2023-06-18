e630041d78b8ca232ecb1d843e73edc54ac2ca40
JansenSmith <jansen@bstarengineering.com>
2023-06-18 19:42:39 -0400

    Tmp save during fork

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,24 +1,32 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Hexagon
+
 CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
 
 //create a Cylinder
-CSG myCylinder = new Cylinder(30, // Radius at the bottom
+CSG myCylinder = new Cylinder(10, // Radius at the bottom
                       		20, // Radius at the top
                       		40, // Height
                       		(int)30 //resolution
-                      		).toCSG()//convert to CSG to display
+                      		).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
                       		.movey(50)
-  //create a Cone
+  //create a Cylinder
 CSG pyramid = new Cylinder(	20, // Radius at the bottom
                       		0, // Radius at the top
                       		40, // Height
                       		(int)4 //resolution
                       		).toCSG()//convert to CSG to display                    			 
                       		.movex(50)
-                      		
-   //create a Hexagonal Pyramid
+   //create a Cylinder
 CSG hex = new Hexagon(	20, // Flat to flat radius
                       		40 // Height
                       		).toCSG()//convert to CSG to display                    			 
                       		.movex(50)
                       		.movey(50)
- return [simpleSyntax,myCylinder ,pyramid,hex]
+hex.setName("hex")
+simpleSyntax.setName("simpleSyntax")
+myCylinder.setName("myCylinder")
+pyramid.setName("pyramid")
+
+return [simpleSyntax,myCylinder ,pyramid,hex]
